0e4a644e1bddf27ced5e97e3cb8748d3ac865214
kaiser82 <dr.kaiserahmed@gmail.com>
2022-10-25 09:18:55 +0600

    private route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,16 +3,19 @@ import Blog from "../components/Blog/Blog";
 import CheckOut from "../components/CheckOut/CheckOut";
 import CourseDetails from "../components/CourseDetails/CourseDetails";
 import Courses from "../components/Courses/Courses";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Home from "../components/Home/Home";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
 import Main from "../Layouts/Main";
+import PrivateRoute from "./PrivateRoute";
 
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        // errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
@@ -24,7 +27,7 @@ export const routes = createBrowserRouter([
             },
             {
                 path: '/courseDetails',
-                element: <CourseDetails></CourseDetails>
+                element: <PrivateRoute><CourseDetails></CourseDetails></PrivateRoute>
             },
             {
                 path: '/checkout',
@@ -43,5 +46,10 @@ export const routes = createBrowserRouter([
                 element: <Blog></Blog>
             },
         ],
+    },
+    {
+        path: '*',
+        element: <ErrorPage></ErrorPage>
     }
+
 ])
